5e855852417ec9d5a84ff646c37edb9969bbf4fc
jay riku <cyouheiwanocyuni@gmail.com>
2021-01-07 23:42:49 +0800

    Spring Bean 实例化后阶段 && 属性赋值前阶段

--- thingking-in-spring/bean-lifecycle/src/main/java/org/geekbang/thinking/in/spring/ioc/bean/lifecycle/BeanInstantiationLifecycleDemo.java
+++ thingking-in-spring/bean-lifecycle/src/main/java/org/geekbang/thinking/in/spring/ioc/bean/lifecycle/BeanInstantiationLifecycleDemo.java
@@ -3,11 +3,11 @@ package org.geekbang.thinking.in.spring.ioc.bean.lifecycle;
 import org.geekbang.thinking.in.spring.ioc.abs.dependency.domain.SuperUser;
 import org.geekbang.thinking.in.spring.ioc.abs.dependency.domain.User;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.MutablePropertyValues;
+import org.springframework.beans.PropertyValues;
 import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.util.ObjectUtils;
 
 /**
@@ -46,6 +46,36 @@ public class BeanInstantiationLifecycleDemo {
                 return superUser; // 返回一个自定义的普通示例，覆盖
             }
             return null; // 保持不变
+        }
+
+        @Override
+        public boolean postProcessAfterInstantiation(Object bean, String beanName) throws BeansException {
+            if (ObjectUtils.nullSafeEquals("user", beanName) && User.class.equals(bean.getClass())) {
+                // 不允许属性赋值 - 属性赋值被跳过
+                return false;
+            }
+            return true;
+        }
+
+        @Override
+        public PropertyValues postProcessProperties(PropertyValues pvs, Object bean, String beanName) throws BeansException {
+            if (ObjectUtils.nullSafeEquals("userHolder", beanName) && UserHolder.class.equals(bean.getClass())) {
+                MutablePropertyValues propertyValues;
+                if (pvs instanceof MutablePropertyValues) {
+                    propertyValues = (MutablePropertyValues) pvs;
+                } else {
+                    propertyValues = new MutablePropertyValues();
+                }
+                // number 配置
+                propertyValues.addPropertyValue("number", "1");
+
+                if (propertyValues.contains("description")) {
+                    propertyValues.removePropertyValue("description");
+                    propertyValues.addPropertyValue("description", "the user holder v2");
+                }
+                return propertyValues;
+            }
+            return null;
         }
     }
 }
--- thingking-in-spring/bean-lifecycle/src/main/java/org/geekbang/thinking/in/spring/ioc/bean/lifecycle/UserHolder.java
+++ thingking-in-spring/bean-lifecycle/src/main/java/org/geekbang/thinking/in/spring/ioc/bean/lifecycle/UserHolder.java
@@ -10,6 +10,8 @@ import org.geekbang.thinking.in.spring.ioc.abs.dependency.domain.User;
  */
 public class UserHolder {
     private final User user;
+    private Integer number;
+    private String description;
 
     public UserHolder(User user) {
         this.user = user;
@@ -19,6 +21,28 @@ public class UserHolder {
     public String toString() {
         return "UserHolder{" +
                 "user=" + user +
+                ", number=" + number +
+                ", description='" + description + '\'' +
                 '}';
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
